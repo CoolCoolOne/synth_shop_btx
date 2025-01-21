94a548353dffa08122f9d062f4bba16451d415da
Troitsky Aleksey <Aleksey-1998@yandex.ru>
2025-01-21 15:47:25 +0300

    персональный раздел, +почтовая настройка
    /personal/

--- local/templates/.default/components/bitrix/menu/custom_main_menu/template.php
+++ local/templates/.default/components/bitrix/menu/custom_main_menu/template.php
@@ -2,17 +2,29 @@
 
 <? if (empty($arResult)) return; ?>
 
-<nav id="nav">
-	<ul>
-		<? foreach ($arResult as $arItem): ?>
 
-			<? if ($arItem["PERMISSION"] > "D"): ?>
-				<? if ($arItem["SELECTED"]): ?>
-					<li class="current"> <a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
-				<? else: ?>
-					<li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
-				<? endif ?>
-			<? endif ?>
-		<? endforeach ?>
-	</ul>
+
+<nav class="ps-1 navbar navbar-expand-lg navbar-dark bg-dark pt-3 ">
+	<div class="container-fluid">
+		<a class="navbar-brand fw-bold" href="#">Волшебство звука</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+			aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse fw-bold" id="navbarNav">
+			<ul class="navbar-nav">
+
+				<? foreach ($arResult as $arItem): ?>
+
+					<? if ($arItem["PERMISSION"] > "D"): ?>
+						<? if ($arItem["SELECTED"]): ?>
+							<li class="nav-item"> <a class="nav-link active" href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+						<? else: ?>
+							<li class="nav-item"> <a class="nav-link" href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+						<? endif ?>
+					<? endif ?>
+				<? endforeach ?>
+			</ul>
+		</div>
+	</div>
 </nav>
